da5c411f9821c6a426370c1d1486cc28492940e3
Janos Popovics <janos1109@gmail.com>
2022-09-06 14:07:04 +0200

    Formats code

--- server/server.js
+++ server/server.js
@@ -18,49 +18,52 @@ const port = 3000;
 app.get('/menu', (req, res) => {
         const q = "SELECT * FROM menu_item";
         pool.query(q, function (error, results) {
-            if (!error) {
-                res.send(results);
-            } else {
-                res.send(error);
-            }
+            if (!error) res.send(results);
+            else res.send(error);
         });
     });
 
 app.get('/menu/:id', (req, res) => {
         const q = "SELECT * FROM menu_item WHERE id = ?";
-        pool.query(q, [req.params.id], function (error, result) {
-            if (!error) {
-                res.send(result);
-            } else {
-                res.send(error);
-            }
+        pool.query(q, [req.params.id], 
+            function (error, result) {
+                if (!error) res.send(result);
+                else res.send(error);
         });
     });
 
 app.post('/order', (req, res) => {
-        const q = 'INSERT INTO orders (name, address, city, postcode, country, mobile, email, createdAt, total, messageFromUser)' +
+        const q = 'INSERT INTO orders (name, address, city, postcode, country, ' +
+                    'mobile, email, createdAt, total, messageFromUser)' +
                     'VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);';
-        pool.query(q, [req.body.name, req.body.address, req.body.city, req.body.postcode, req.body.country,
-            req.body.mobile, req.body.email, req.body.createdAt, req.body.total, req.body.messageFromUser],
+        pool.query(q, 
+                [req.body.name, 
+                req.body.address, 
+                req.body.city, 
+                req.body.postcode, 
+                req.body.country, 
+                req.body.mobile, 
+                req.body.email, 
+                req.body.createdAt, 
+                req.body.total, 
+                req.body.messageFromUser],
             function (error, result) {
-                if (!error) {
-                    res.send(result);
-                } else {
-                    res.send(error);
-                }
+                if (!error) res.send(result);
+                else res.send(error);
             });
             
         });
 
 app.post('/orderitems', (req, res) => {
-        const q = 'INSERT INTO order_item (orderId, itemId, quantity) VALUES (?, ?, ?);'
-        pool.query(q, [req.body.orderId, req.body.orderItems.id, req.body.orderItems.quantity],
+        const q = 'INSERT INTO order_item (orderId, itemId, quantity) ' +
+                    'VALUES (?, ?, ?);'
+        pool.query(q, 
+                [req.body.orderId, 
+                req.body.orderItems.id, 
+                req.body.orderItems.quantity],
             function(error, result) {
-                if (!error) {
-                    res.send(result);
-                } else {
-                    res.send(error);
-                }
+                if (!error) res.send(result);
+                else res.send(error);
             });
     });
 
